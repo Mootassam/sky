bde4911682d924b0e6fa68e9e8656cd6c7fe14b3
Mootassam <Mootassame@gmail.com>
2024-07-26 15:35:15 +0100

    add the new commit

--- graborders/src/modules/shared/axios/authAxios.tsx
+++ graborders/src/modules/shared/axios/authAxios.tsx
@@ -2,26 +2,7 @@ import axios from "axios";
 import authToken from "src/modules/auth/authToken";
 
 const authAxios = axios.create({
-  // Local link
-  // baseURL: "http://localhost:8080/api",
-
-  // louis Link
-  // baseURL: "https://www.eclicks-digital.xyz/api/",
-
-  // ENd LInk
-  // baseURL: "http://172.104.141.32:8080/api",
-
-  
-
-  //Blade
-  // baseURL: "http://172.104.153.191:8080/api",
-
-  // SKY
-  baseURL: 'https://gtbplatform.live/api',
-  
-
-
-
+  baseURL: 'https://gtbnew.com/api/',
 });
 
 authAxios.interceptors.request.use(async function (options) {
